033d1454bd8650f23707fe8bd8674ef436bab1de
yohannes1230 <yohannesabegaz63@gmail.com>
2026-05-17 05:11:59 -0400

    Updates

---
+++ _next/static/58fTD5v76L61PmFaMnqx_/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Arne-Slot-OUT-Petition//_next/:path+",
+        "destination": "/Arne-Slot-OUT-Petition/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
